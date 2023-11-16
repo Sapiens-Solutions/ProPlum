f5f09423b89d1ea359244b562488a12a96a8410b
Dmitry Ismailov <d.ismailov@sapiens.solutions>
2023-11-16 16:57:15 +0300

    f_create_date_partitions - add installation variables

--- fw/functions/f_create_date_partitions(text,timestamp).sql
+++ fw/functions/f_create_date_partitions(text,timestamp).sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION fw.f_create_date_partitions(p_table_name text, p_partition_value timestamp)
+CREATE OR REPLACE FUNCTION ${target_schema}.f_create_date_partitions(p_table_name text, p_partition_value timestamp)
 	RETURNS void
 	LANGUAGE plpgsql
 	VOLATILE
@@ -8,7 +8,7 @@ AS $$
     * 2023*/
 /*Function create new partition for table*/
 DECLARE
-  v_location            text := 'fw.f_create_date_partitions';
+  v_location            text := '${target_schema}.f_create_date_partitions';
   v_cnt_partitions      int;
   v_table_name          text;
   v_partition_name      text;
@@ -22,18 +22,18 @@ DECLARE
 BEGIN
 
   -- Unify parameters
-  v_table_name = fw.f_unify_name(p_table_name);
+  v_table_name = ${target_schema}.f_unify_name(p_table_name);
 
   --Log
-  PERFORM fw.f_write_log(
+  PERFORM ${target_schema}.f_write_log(
      p_log_type    := 'SERVICE', 
      p_log_message := 'START Creating partitions for table '||v_table_name, 
      p_location    := v_location);
-  PERFORM fw.f_write_log('DEBUG', 'v_table_name:{'||v_table_name||'}', v_location);
+  PERFORM ${target_schema}.f_write_log('DEBUG', 'v_table_name:{'||v_table_name||'}', v_location);
 
   IF p_partition_value is null THEN
       v_error := 'Partition value should not be null';
-      PERFORM fw.f_write_log(
+      PERFORM ${target_schema}.f_write_log(
          p_log_type    := 'ERROR', 
          p_log_message := 'Error while creating partition in table '||p_table_name||':'||v_error, 
          p_location    := v_location);
@@ -59,11 +59,11 @@ BEGIN
               ) q
           where rnk = 1;
       
-      PERFORM fw.f_write_log(
+      PERFORM ${target_schema}.f_write_log(
          p_log_type    := 'DEBUG', 
          p_log_message := 'v_partition_end_sql:{'||v_partition_end_sql||'}', 
          p_location    := v_location);
-      PERFORM fw.f_write_log(
+      PERFORM ${target_schema}.f_write_log(
          p_log_type    := 'DEBUG', 
          p_log_message := 'v_partition_delta_sql:{'||v_partition_delta_sql||'}', 
          p_location    := v_location);
@@ -72,11 +72,11 @@ BEGIN
       EXECUTE 'SELECT '||v_partition_end_sql INTO v_partition_end;
       EXECUTE 'SELECT '||v_partition_delta_sql INTO v_partition_delta;
          
-      PERFORM fw.f_write_log(
+      PERFORM ${target_schema}.f_write_log(
          p_log_type    := 'DEBUG', 
          p_log_message := 'v_partition_end:{'||v_partition_end||'}', 
          p_location    := v_location);
-      PERFORM fw.f_write_log(
+      PERFORM ${target_schema}.f_write_log(
          p_log_type    := 'DEBUG', 
          p_log_message := 'v_partition_delta:{'||v_partition_delta||'}', 
          p_location    := v_location);
@@ -103,11 +103,11 @@ BEGIN
         v_partition_name := 'y_'||v_partition_name;
       ELSE
         v_error := 'Unable to define partition interval ';
-        PERFORM fw.f_write_log('ERROR', 'Error while creating partition in table '||p_table_name||':'||v_error, v_location);
+        PERFORM ${target_schema}.f_write_log('ERROR', 'Error while creating partition in table '||p_table_name||':'||v_error, v_location);
         RAISE EXCEPTION '% for table % partition %',v_error, v_table_name,v_partition_end_sql;
       END IF;
 
-      PERFORM fw.f_write_log(
+      PERFORM ${target_schema}.f_write_log(
          p_log_type    := 'DEBUG', 
          p_log_message := 'v_interval:{'||v_interval||'}', 
          p_location    :=  v_location);
@@ -115,21 +115,21 @@ BEGIN
       EXECUTE 'ALTER TABLE '||v_table_name||' SPLIT DEFAULT PARTITION START ('||v_partition_end_sql||') END ('''||to_char(v_partition_end+v_interval, v_ts_format)||'''::timestamp)
       INTO (PARTITION '||v_partition_name||', default partition)';
      
-      PERFORM fw.f_write_log(
+      PERFORM ${target_schema}.f_write_log(
          p_log_type    := 'SERVICE', 
          p_log_message := 'Created partition '||v_partition_end_sql||' for table '||v_table_name, 
          p_location    := v_location);
 
     END LOOP;
   ELSE
-      PERFORM fw.f_write_log(
+      PERFORM ${target_schema}.f_write_log(
          p_log_type    := 'SERVICE', 
          p_log_message := 'Table is not partitioned '||v_table_name, 
          p_location    := v_location);
   End if;
 
   -- Log Success
-  PERFORM fw.f_write_log(
+  PERFORM ${target_schema}.f_write_log(
      p_log_type    :='SERVICE', 
      p_log_message := 'END Created partitions for table '||v_table_name, 
      p_location    := v_location);
