ed7d5680658876dc44cf05475fe558f9ded8a39e
Dmitry Ismailov <d.ismailov@sapiens.solutions>
2024-07-17 10:56:45 +0300

    separate stage for objects with same name, but different object_id

--- fw/functions/f_get_ext_table_name(int8).sql
+++ fw/functions/f_get_ext_table_name(int8).sql
@@ -13,6 +13,7 @@ DECLARE
   v_ext_table_name text;
   v_table_name text;
   v_schema_name text;
+  v_object_id int8;
   v_sql       text;
 BEGIN
   perform ${target_schema}.f_write_log(
@@ -21,18 +22,17 @@ BEGIN
      p_location    := v_location,
      p_load_id     := p_load_id); --log function call
 
-  v_sql := 'select ob.object_name
+  v_sql := 'select ob.object_id, ob.object_name
             from ${target_schema}.load_info li, ${target_schema}.objects ob where li.object_id = ob.object_id and li.load_id = ' ||
             p_load_id::text;
-  execute v_sql into v_full_table_name; 
+  execute v_sql into v_object_id, v_full_table_name; 
 
   v_full_table_name  = ${target_schema}.f_unify_name(p_name := v_full_table_name); -- full table name
   v_schema_name = ${target_schema}.f_get_table_schema(v_full_table_name); --left(v_full_table_name,position('.' in v_full_table_name)-1); -- target table schema name
-  v_schema_name = replace(replace(v_schema_name,'src_',''),'stg_','');
+  v_schema_name = replace(replace(replace(v_schema_name,'src_',''),'stg_',''),'load_','');
   v_schema_name = coalesce(${target_schema}.f_get_constant('c_stg_table_schema'),'stg_')||v_schema_name;-- delta table schema name
   v_table_name =  right(v_full_table_name,length(v_full_table_name) - POSITION('.' in v_full_table_name));-- table name wo schema
-  v_ext_table_name = v_schema_name||'.'||coalesce(${target_schema}.f_get_constant('c_ext_table_prefix'),'ext_')||
-                     v_table_name ||decode(coalesce(${target_schema}.f_get_constant(p_constant_name := 'c_load_id_stage')::bool, false),true,'_'||p_load_id,'');
+  v_ext_table_name = v_schema_name||'.'||coalesce(${target_schema}.f_get_constant('c_ext_table_prefix'),'ext_')||v_table_name||'_'||v_object_id;--||'_'||p_load_id;
  
   perform ${target_schema}.f_write_log(
      p_log_type    := 'SERVICE', 
@@ -41,7 +41,6 @@ BEGIN
      p_load_id     := p_load_id); --log function call
   return v_ext_table_name;
 END;
-
 $$
 EXECUTE ON ANY;
 
